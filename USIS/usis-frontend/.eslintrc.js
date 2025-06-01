50b41ce75134c520604f0ed25d215bb69956e229
Wasif <md.wasif.faisal@g.bracu.ac.bd>
2025-06-01 23:22:04 +0600

    Update ESLint config to handle TypeScript and empty blocks

--- USIS/usis-frontend/.eslintrc.js
+++ USIS/usis-frontend/.eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -16,15 +18,27 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
     'no-unused-vars': 'off', // Turn off the unused variables warning
     'react/react-in-jsx-scope': 'off', // Not needed in React 17+
     'react/prop-types': 'off', // Turn off prop-types validation
+    'no-empty': 'off', // Disable empty block statement warning
+    'no-undef': 'off', // Disable undefined variable warnings for TypeScript files
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off'
+      }
+    }
+  ]
 }; 
